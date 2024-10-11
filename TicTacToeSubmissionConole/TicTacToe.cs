90f06b33ae3057b88cfd6491a092a4e92cfae304
Dober09 <lebohanglehutjo@gmail.com>
2024-10-11 10:57:18 +0200

    modified  TicTacToe by adding checking which player is playying

--- TicTacToeSubmissionConole/TicTacToe.cs
+++ TicTacToeSubmissionConole/TicTacToe.cs
@@ -20,28 +20,42 @@ namespace TicTacToeSubmissionConole
         public void Run()
         {
 
-            // FOR ILLUSTRATION CHANGE TO YOUR OWN LOGIC TO DO TIC TAC TOE
-          
+            // couter for the number of times your are allowed to play
+            int counter = 1;
+            // The two Players
+            
 
-            Console.SetCursorPosition(2, 19);
-
-            Console.Write("Player X");
-
-            Console.SetCursorPosition(2, 20);
-
-            Console.Write("Please Enter Row: ");
-            var row = Console.ReadLine();
-
-            Console.SetCursorPosition(2, 22);
+            
+            while(counter < 7)
+            {
 
 
-            Console.Write("Please Enter Column: ");
-            var column = Console.ReadLine();
+                Console.SetCursorPosition(2, 19);
+
+                //tenary if statement for checking which player should play
+                Console.Write($"Player {(counter%2==0 ? "O" :"X" )}");
+
+                Console.SetCursorPosition(2, 20);
+
+                Console.Write("Please Enter Row: ");
+                var row = Console.ReadLine();
+
+                Console.SetCursorPosition(2, 22);
 
 
-            // THIS JUST DRAWS THE BOARD (NO TIC TAC TOE LOGIC)
-            _boardRenderer.AddMove(int.Parse(row), int.Parse(column), PlayerEnum.X, true);            
+                Console.Write("Please Enter Column: ");
+                var column = Console.ReadLine();
 
+
+                // THIS JUST DRAWS THE BOARD (NO TIC TAC TOE LOGIC)
+                //tenary if statement for checking which player should play
+                var player = counter % 2 == 0 ? PlayerEnum.O : PlayerEnum.X;
+                _boardRenderer.AddMove(int.Parse(row), int.Parse(column), player, true);             
+
+                //increment the counter 
+                counter++;
+
+            }
         }
 
     }
